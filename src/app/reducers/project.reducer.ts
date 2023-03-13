5f3e531e6ba603d2f2d4fd71acf965472c2f5680
mcaprema1 <mcaprema1@gmail.com>
2023-03-13 15:30:24 +0530

    initial commit1

--- src/app/reducers/project.reducer.ts
+++ src/app/reducers/project.reducer.ts
@@ -12,17 +12,12 @@ export function projectreducer(state: Projects[] = [initialState], action: Actio
     const tutorialAction = action as DemoActions.Actions; 
     console.log("reducers projectreducer: ", tutorialAction.type)
     switch(tutorialAction.type) {
-        // case DemoActions.ADD_DEMO:
-        //     return [...state, tutorialAction.payload];
-        // case DemoActions.REMOVE_DEMO:
-        //     state.splice(tutorialAction.payload, 1);
-        //     return state;
         case DemoActions.ADD_PROJECT:
             return [...state, tutorialAction.payload];
         case DemoActions.UPDATE_PROJECT: {
-            // export const getSelectedActivity = (id: string) =>
-        //     createSelector(state, getSelectedActivityId, (state) => {
-        //         return state.activities.find((activity) => activity.id === id);
+            // export const getSelectedrecord = (id: string) =>
+        //     createSelector(state, getSelectedId, (state) => {
+        //         return state.find((data) => data.id === id);
         //     });
         }
             return [...state, tutorialAction.payload];
